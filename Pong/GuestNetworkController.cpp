387976fd9e56371cea7ade7b163ef20c77427294
WollyTech <ayolawole@gmail.com>
2025-12-02 16:10:48 +0000

    recieving contact from the host

--- Pong/GuestNetworkController.cpp
+++ Pong/GuestNetworkController.cpp
@@ -22,12 +22,51 @@ bool GuestNetworkController::bind(unsigned short port)
 
 void GuestNetworkController::sendFindHost(unsigned short discoveryPort)
 {
+	// Build FIND_HOST packet (message type 1)
+	uint8_t msg = MessageTypes::FIND_HOST;
 
+	sf::Socket::Status status = m_socket.send(&msg, sizeof(msg), sf::IpAddress::Broadcast, discoveryPort);
+
+	if(status!= sf::Socket::Status::Done)
+	{
+		cout << "GuestNetworkController: Failed to send FIND_HOST (status "
+			<< static_cast<int>(status) << ")" << endl;
+		return;
+	}
 }
 
 bool GuestNetworkController::recieveHostHere(sf::IpAddress& outAddress, unsigned short& outPort)
 {
-	return false;
+	Buffer buffer;
+	Socket::Status status = m_socket.receive(buffer.data, sizeof(buffer.data), buffer.recieved, buffer.sender, buffer.senderPort);
+
+	// ---- ERROR CHECKS ----
+	if (status != Socket::Status::Done)
+		return false;
+
+	if (!buffer.sender.has_value() || buffer.recieved < 3)
+		return false;
+
+	uint8_t msgType = buffer.data[0];
+	if (msgType != MessageTypes::HOST_HERE)
+		return false;
+
+	// ---- Extract host port (from bytes 1 and 2, big-endian) ----
+	uint8_t hi = static_cast<uint8_t>(buffer.data[1]);
+	uint8_t lo = static_cast<uint8_t>(buffer.data[2]);
+	unsigned short hostPort =
+		(static_cast<unsigned short>(hi) << 8) |
+		static_cast<unsigned short>(lo);
+
+/*potential point of failure!*/ m_hostAddress = buffer.sender.value();
+	m_hostPort = hostPort;
+
+	outAddress = m_hostAddress;
+	outPort = m_hostPort;
+
+	cout << "GuestNetworkController: Found host at "
+		<< m_hostAddress.toString() << ":" << m_hostPort << endl;
+	return true;
 }
 
 void GuestNetworkController::sendHello()
