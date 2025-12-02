8763b1c7f7f6c044ffc0d96adc64194b90fc9ee6
WollyTech <ayolawole@gmail.com>
2025-12-02 11:26:13 +0000

    Looking/Waiting for Guest to connect

--- Pong/HostNetworkController.cpp
+++ Pong/HostNetworkController.cpp
@@ -63,3 +63,58 @@ void HostNetworkController::pollForDiscoveryRequests()
 			<< " -> sent from HOST_HERE\n" << endl;
 	}
 }
+
+bool HostNetworkController::pollForHello()
+{
+	// only proceed if we haven't accepted a guest yet
+	if (m_hasGuest)
+		return false;
+
+	//Buffer for incoming data
+	char data[16];
+	size_t recieved = 0;
+	std::optional<sf::IpAddress> sender;
+	unsigned short senderPort = 0;
+
+	auto status = m_socket.receive(data, sizeof(data), recieved, sender, senderPort);
+
+	// ---- ERROR CHECKS ----
+
+	if (status != Socket::Status::Done)
+		return false;
+	if (!sender.has_value() || recieved < 1)
+		return false;
+
+	uint8_t messageType = data[0];
+	if(messageType != MessageTypes::HELLO)
+		return false;
+
+	// The HELLO packet should contain the gameplay recieve port for the guest
+	if (recieved < 3)
+		return false; // invalid HELLO packet
+
+	// ---- Extract guest port (from bytes 1 and 2, big-endian) ----
+	unsigned short guestPort =
+		(static_cast<uint8_t>(data[1]) << 8) |
+		static_cast<uint8_t>(data[2]);
+
+	m_guestAddress = sender.value();
+	m_guestPort = guestPort;
+	m_hasGuest = true;
+
+	// ---- Send HELLO_ACK ----
+	uint8_t reply[1];
+	reply[0] = MessageTypes::HELLO_ACK;
+
+	auto sendStatus = m_socket.send(reply, sizeof(reply), m_guestAddress, m_guestPort);
+	if (sendStatus != Socket::Status::Done)
+	{
+		cout << "HostNetworkController: Failed to send HELLO_ACK to "
+			<< m_guestAddress.toString() << ":" << m_guestPort << endl;
+		m_hasGuest = false; // reset guest info
+		return false;
+	}
+	cout << "HostNetworkController: Guest connected from "
+		<< m_guestAddress.toString() << ":" << m_guestPort <<
+		" -> HELLO_ACK sent" << endl;
+}
